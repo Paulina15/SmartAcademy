6719524baadca0c4d978124d67a205b122bd179a
Dalia <54043172+DaliaVazquez@users.noreply.github.com>
2022-07-23 02:19:41 -0500

    Add files via upload

---
+++ application/shared_components/logic/build_options.sql
@@ -0,0 +1,21 @@
+prompt --application/shared_components/logic/build_options
+begin
+--   Manifest
+--     BUILD OPTIONS: 103
+--   Manifest End
+wwv_flow_imp.component_begin (
+ p_version_yyyy_mm_dd=>'2022.04.12'
+,p_release=>'22.1.2'
+,p_default_workspace_id=>7870384460919289
+,p_default_application_id=>104
+,p_default_id_offset=>30137121571976061
+,p_default_owner=>'SACALABICI'
+);
+wwv_flow_imp_shared.create_build_option(
+ p_id=>wwv_flow_imp.id(8089712700772778)
+,p_build_option_name=>'Commented Out'
+,p_build_option_status=>'EXCLUDE'
+);
+wwv_flow_imp.component_end;
+end;
+/
